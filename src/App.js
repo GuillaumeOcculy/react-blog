58624bbb74d59858f2ab0f2366196d21915d1420
Guillaume Occuly <occuly.guillaume@gmail.com>
2020-03-30 22:24:25 +0200

    Add Home and About pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <div class="ui inverted segment">
+        <div class="ui inverted secondary menu container">
+          <Link to="/" className="item">
+            Home
+          </Link>
+          <Link to="/about" className="item">
+            About
+          </Link>
+        </div>
+      </div>
+
+      <div className="ui container">
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
