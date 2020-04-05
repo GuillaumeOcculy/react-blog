1e30fa4b9198a36ca2b71c77d3218957b0feb9ca
Guillaume Occuly <occuly.guillaume@gmail.com>
2020-04-05 16:19:33 +0200

    User can unlike post

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -8,7 +8,7 @@ import MOT from "./../apis/MOT";
 function PostDetail(props, { creator }) {
   const [post, setPost] = useState(props.post);
 
-  const { body, created_at } = post.attributes;
+  const { body, created_at, liked_by_current_user } = post.attributes;
   const likes = post.relationships.likes.data;
 
   function renderUser() {
@@ -17,8 +17,30 @@ function PostDetail(props, { creator }) {
     }
   }
 
+  function renderLikeIcon() {
+    return <i className="heart outline like icon" onClick={handleLike}></i>;
+  }
+
+  function renderUnlikeIcon() {
+    return <i className="heart red like icon" onClick={handleUnlike}></i>;
+  }
+
+  function renderLikeButton() {
+    if (liked_by_current_user) {
+      return renderUnlikeIcon();
+    } else {
+      return renderLikeIcon();
+    }
+  }
+
   async function handleLike() {
     const response = await MOT.post(`/posts/${post.id}/likes`);
+
+    setPost(response.data.data);
+  }
+
+  async function handleUnlike() {
+    const response = await MOT.delete(`/posts/${post.id}/likes`);
 
     setPost(response.data.data);
   }
@@ -38,7 +60,7 @@ function PostDetail(props, { creator }) {
       </div>
       <div className="content">
         <span className="right floated">
-          <i className="heart outline like icon" onClick={handleLike}></i>
+          {renderLikeButton()}
           {likes.length} likes
         </span>
         <i className="comment icon"></i>3 comments
