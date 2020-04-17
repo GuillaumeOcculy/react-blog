a606d324f2fc46296c1ef1914e43f53f80729821
Guillaume Occuly <occuly.guillaume@gmail.com>
2020-04-18 00:09:40 +0200

    Refactor Profile

--- src/components/UserFriendshipButton.js
+++ src/components/UserFriendshipButton.js
@@ -1,32 +1,35 @@
 import React from "react";
 import { Button } from "semantic-ui-react";
 
-function UserFriendshipButton({
-  status,
+const UserFriendshipButton = ({
+  isUser,
   handleSendFriendRequest,
   friendshipButtonClicked,
-}) {
-  function renderButton() {
-    switch (status) {
-      case "accepted":
-        return null;
-      case "pending":
-        return <Button disabled>Pending</Button>;
-      case "declined":
-        return <Button disabled>Declined</Button>;
-      default:
-        return (
-          <Button
-            onClick={handleSendFriendRequest}
-            disabled={friendshipButtonClicked}
-          >
-            Send friend request
-          </Button>
-        );
-    }
+  user,
+}) => {
+  const friendshipStatus = user.attributes.current_user_friendship_status;
+
+  if (isUser) {
+    return null;
   }
 
-  return renderButton();
-}
+  switch (friendshipStatus) {
+    case "accepted":
+      return null;
+    case "pending":
+      return <Button disabled>Pending</Button>;
+    case "declined":
+      return <Button disabled>Declined</Button>;
+    default:
+      return (
+        <Button
+          onClick={handleSendFriendRequest}
+          disabled={friendshipButtonClicked}
+        >
+          Send friend request
+        </Button>
+      );
+  }
+};
 
 export default UserFriendshipButton;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import { useParams } from "react-router-dom";
-import { Loader } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Menu, Loader } from "semantic-ui-react";
 
 import { AuthContext } from "../contexts/AuthContext";
 import useFetchUserApi from "./../hooks/useFetchUserApi";
@@ -14,35 +15,54 @@ function Profile() {
   const { user_id } = useParams();
   const [{ user, isLoading, isError }] = useFetchUserApi(user_id);
   const [friendshipButtonClicked, setFriendshipButtonClicked] = useState(false);
+  const isUser = user && user.id === context.currentUserId();
 
-  function renderUser() {
+  const RenderProfile = () => {
+    return (
+      <div>
+        <RenderMenu />
+        <RenderUser />
+      </div>
+    );
+  };
+
+  const RenderMenu = () => {
+    if (!user) {
+      return null;
+    }
+
+    const { username } = user.attributes;
+
+    return (
+      <Menu secondary>
+        <Menu.Item name="home" active={true} as={Link} to={`/@${username}`} />
+        <Menu.Item name="posts" as={Link} to={`/@${username}/posts`} />
+        <Menu.Item name="friends" as={Link} to={`/@${username}/friends`} />
+        <Menu.Item name="messages" as={Link} to={`/@${username}/messages`} />
+      </Menu>
+    );
+  };
+
+  const RenderUser = () => {
     if (user) {
       return (
         <div>
-          <div>
-            <UserDetail user={user} />
-          </div>
+          <UserDetail user={user} />
 
-          <div>{renderFriendshipButton()}</div>
+          <br />
+
+          <UserFriendshipButton
+            isUser={isUser}
+            user={user}
+            friendshipButtonClicked={friendshipButtonClicked}
+            handleSendFriendRequest={handleSendFriendRequest}
+          />
         </div>
       );
     }
 
     return null;
-  }
-
-  function renderFriendshipButton() {
-    if (user && user.id != context.currentUserId()) {
-      const friendshipStatus = user.attributes.current_user_friendship_status;
-      return (
-        <UserFriendshipButton
-          status={friendshipStatus}
-          friendshipButtonClicked={friendshipButtonClicked}
-          handleSendFriendRequest={handleSendFriendRequest}
-        />
-      );
-    }
-  }
+  };
 
   const handleSendFriendRequest = async () => {
     await BlogAPI.post("/friendships", {
@@ -50,7 +70,6 @@ function Profile() {
     })
       .then(function (response) {
         if (response.status === 201) {
-          console.log(response);
           setFriendshipButtonClicked(true);
         } else {
           console.log(response);
@@ -64,7 +83,11 @@ function Profile() {
   return (
     <div className="ui two column centered grid">
       {isError && <div>Something went wrong ...</div>}
-      {isLoading ? <Loader active inline="centered" /> : renderUser()}
+      {isLoading ? (
+        <Loader active inline="centered" />
+      ) : (
+        <RenderProfile user={user} isUser={isUser} />
+      )}
     </div>
   );
 }
