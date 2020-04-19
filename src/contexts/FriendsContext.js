3bf53d525e0277e36e5c99170b0fb48269dac6cc
Guillaume Occuly <occuly.guillaume@gmail.com>
2020-04-19 14:35:27 +0200

    Refactor FriendsContext by using useFetchFriendApi hooks

--- src/contexts/FriendsContext.js
+++ src/contexts/FriendsContext.js
@@ -1,81 +1,20 @@
-import React, { createContext, useState, useEffect } from "react";
-import BlogAPI from "../apis/BlogAPI";
+import React, { createContext, useState } from "react";
+import useFetchFriendsApi from "../hooks/useFetchFriendsApi";
 
 const FriendsContext = createContext();
 
 const FriendsContextProvider = ({ children }) => {
   const [username, setUsername] = useState();
-  const [friends, setFriends] = useState([]);
-  const [friendRequests, setFriendRequests] = useState([]);
-
-  useEffect(() => {
-    console.log("useEffect", username);
-    if (username === undefined) {
-      return;
-    }
-
-    async function fetchFriends() {
-      const response = await BlogAPI.get(`/users/${username}/friends`);
-      const { data } = response.data;
-
-      setFriends(data);
-    }
-
-    async function fetchFriendRequests() {
-      const response = await BlogAPI.get(`/users/${username}/friend_requests`);
-      const { data } = response.data;
-
-      setFriendRequests(data);
-    }
-
-    fetchFriends();
-    fetchFriendRequests();
-  }, [username, friendRequests.length]);
-
-  const handleAcceptRequest = (e, username) => {
-    e.preventDefault();
-    BlogAPI.patch(`/friend_requests/${username}/accept`)
-      .then(function (response) {
-        if (response.status === 200) {
-          setFriendRequests(response.data.data);
-        } else {
-          console.log(response);
-        }
-      })
-      .catch(function (error) {
-        console.log(JSON.stringify(error.response));
-      });
-  };
-
-  const handleDeclineRequest = (e, username) => {
-    e.preventDefault();
-    BlogAPI.patch(`/friend_requests/${username}/decline`)
-      .then(function (response) {
-        if (response.status === 200) {
-          setFriendRequests(response.data.data);
-        } else {
-          console.log(response);
-        }
-      })
-      .catch(function (error) {
-        console.log(JSON.stringify(error.response));
-      });
-  };
-
-  const handleUnfriend = (e, username) => {
-    e.preventDefault();
-    BlogAPI.delete(`/friends/${username}`)
-      .then(function (response) {
-        if (response.status === 200) {
-          setFriends(response.data.data);
-        } else {
-          console.log(response);
-        }
-      })
-      .catch(function (error) {
-        console.log(JSON.stringify(error.response));
-      });
-  };
+  const [
+    { friends, friendRequests },
+    {
+      setFriends,
+      setFriendRequests,
+      handleAcceptRequest,
+      handleDeclineRequest,
+      handleUnfriend,
+    },
+  ] = useFetchFriendsApi(username);
 
   return (
     <FriendsContext.Provider
---
+++ src/hooks/useFetchFriendsApi.js
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import BlogAPI from "../apis/BlogAPI";
+
+const useFetchFriendsApi = (usernameProps) => {
+  const username = usernameProps;
+  const [friends, setFriends] = useState([]);
+  const [friendRequests, setFriendRequests] = useState([]);
+
+  useEffect(() => {
+    if (username === undefined) {
+      return;
+    }
+
+    async function fetchFriends() {
+      const response = await BlogAPI.get(`/users/${username}/friends`);
+      const { data } = response.data;
+
+      setFriends(data);
+    }
+
+    async function fetchFriendRequests() {
+      const response = await BlogAPI.get(`/users/${username}/friend_requests`);
+      const { data } = response.data;
+
+      setFriendRequests(data);
+    }
+
+    fetchFriends();
+    fetchFriendRequests();
+  }, [username, friendRequests.length]);
+
+  const handleAcceptRequest = (e, username) => {
+    e.preventDefault();
+    BlogAPI.patch(`/friend_requests/${username}/accept`)
+      .then(function (response) {
+        if (response.status === 200) {
+          setFriendRequests(response.data.data);
+        } else {
+          console.log(response);
+        }
+      })
+      .catch(function (error) {
+        console.log(JSON.stringify(error.response));
+      });
+  };
+
+  const handleDeclineRequest = (e, username) => {
+    e.preventDefault();
+    BlogAPI.patch(`/friend_requests/${username}/decline`)
+      .then(function (response) {
+        if (response.status === 200) {
+          setFriendRequests(response.data.data);
+        } else {
+          console.log(response);
+        }
+      })
+      .catch(function (error) {
+        console.log(JSON.stringify(error.response));
+      });
+  };
+
+  const handleUnfriend = (e, username) => {
+    e.preventDefault();
+    BlogAPI.delete(`/friends/${username}`)
+      .then(function (response) {
+        if (response.status === 200) {
+          setFriends(response.data.data);
+        } else {
+          console.log(response);
+        }
+      })
+      .catch(function (error) {
+        console.log(JSON.stringify(error.response));
+      });
+  };
+
+  return [
+    { friends, friendRequests },
+    { handleAcceptRequest, handleDeclineRequest, handleUnfriend },
+  ];
+};
+
+export default useFetchFriendsApi;
