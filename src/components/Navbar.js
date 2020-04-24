9fea84ff9b8845ad8378fda5fb468fc6db98e715
Guillaume Occuly <occuly.guillaume@gmail.com>
2020-04-25 01:00:15 +0200

    User can search users

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,47 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, Redirect } from "react-router-dom";
+import { Dropdown, Search, Grid } from "semantic-ui-react";
+import _ from "lodash";
+
 import { AuthContext } from "../contexts/AuthContext";
-import { Dropdown } from "semantic-ui-react";
+import BlogAPI from "../apis/BlogAPI";
 
 function Navbar() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState([]);
+  const [value, setValue] = useState();
+  const [userSelected, setUserSelected] = useState(false);
+
+  const handleResultSelect = (e, { result }) => {
+    setValue(result.title);
+    setUserSelected(true);
+  };
+
+  const RedirectToUser = () => {
+    if (!userSelected) {
+      return null;
+    }
+
+    return <Redirect to={`/@${value}`} />;
+  };
+
+  const handleSearchChange = async (e, { value }) => {
+    setIsLoading(true);
+    setValue(value);
+
+    const response = await BlogAPI.get(`/searches?q=${value}`);
+    const data = response.data.data;
+
+    const users = data.map((element) => {
+      return {
+        title: element.attributes.username,
+      };
+    });
+
+    setResults(users);
+    setIsLoading(false);
+  };
+
   const context = useContext(AuthContext);
 
   function handleLogout() {
@@ -53,9 +91,22 @@ function Navbar() {
         <Link to="/" className="item">
           Home
         </Link>
-        <Link to="/about" className="item">
-          About
-        </Link>
+
+        <Grid>
+          <Grid.Column width={6}>
+            <Search
+              loading={isLoading}
+              onResultSelect={handleResultSelect}
+              onSearchChange={_.debounce(handleSearchChange, 500, {
+                leading: true,
+              })}
+              results={results}
+              value={value}
+            />
+          </Grid.Column>
+        </Grid>
+
+        <RedirectToUser />
 
         <div className="right menu">{renderLinks()}</div>
       </div>
